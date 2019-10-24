963045494f431babc4fb9e5952e67adc4af08c75
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-10-24 16:17:30 +0100

    initial version for document testing

---
+++ src/chembl_webservices/tests/test_document.py
@@ -0,0 +1,22 @@
+from chembl_webservices.tests import BaseWebServiceTestCase
+
+class DocumentTestCase(BaseWebServiceTestCase):
+
+    resource = 'document'
+    id_property = 'document_chembl_id'
+    resource_expected_count = 72271
+    sorting_test_props = ['title']
+    mandatory_properties = [
+      'abstract',
+      'doc_type',
+      'document_chembl_id',
+      'doi',
+      'title',
+    ]
+
+    def test_filtered_lists(self):
+        doc_list_req = self.get_current_resource_list({
+            'doc_type': 'PUBLICATION',
+            'year__gt': 1985,
+        })
+        self.assertGreaterEqual(doc_list_req['page_meta']['total_count'], 66000)
