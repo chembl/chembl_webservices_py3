9831b5b40c7472b0236d3fe739d32b2a6fd353ea
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-11-14 17:10:46 +0000

    initial definition for compound records endpoint test

---
+++ src/chembl_webservices/tests/test_compound_record.py
@@ -0,0 +1,40 @@
+from chembl_webservices.tests import BaseWebServiceTestCase
+
+
+class CompoundRecordTestCase(BaseWebServiceTestCase):
+
+    resource = 'compound_record'
+    id_property = 'record_id'
+    resource_expected_count = 2335417
+    sorting_test_props = ['compound_name']
+    mandatory_properties = [
+      'compound_key',
+      'compound_name',
+      'document_chembl_id',
+      'molecule_chembl_id',
+      'record_id',
+      'src_id',
+    ]
+
+    def test_filtered_lists(self):
+        records_req = self.get_current_resource_list({
+            'compound_name__istartswith': '2-Acetoxy',
+            'compound_key__icontains': 'ASA',
+            'document_chembl_id': 'CHEMBL1123757',
+        })
+        self.assertGreaterEqual(records_req['page_meta']['total_count'], 1)
+
+        records_req = self.get_current_resource_list({
+            'document_chembl_id': 'CHEMBL1126670',
+        })
+        self.assertGreaterEqual(records_req['page_meta']['total_count'], 20)
+
+        records_req = self.get_current_resource_list({
+            'compound_name__iendswith': 'propionamide',
+        })
+        self.assertGreaterEqual(records_req['page_meta']['total_count'], 6000)
+
+        records_req = self.get_current_resource_list({
+            'molecule_chembl_id': 'CHEMBL409',
+        })
+        self.assertGreaterEqual(records_req['page_meta']['total_count'], 70)
