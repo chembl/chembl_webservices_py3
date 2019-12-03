8c7e26670cb791305434402266628722fd4d38b1
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-12-03 17:08:47 +0000

    2to3 run and reviewed

--- src/chembl_core_model/models/general.py
+++ src/chembl_core_model/models/general.py
@@ -10,9 +10,9 @@ from django.utils import six
 
 class Version(six.with_metaclass(ChemblModelMetaClass, ChemblCoreAbstractModel)):
 
-    name = ChemblCharField(primary_key=True, max_length=20, help_text=u'Name of release version')
-    creation_date = ChemblDateField(blank=True, null=True, help_text=u'Date database created')
-    comments = ChemblCharField(max_length=2000, blank=True, null=True, help_text=u'Description of release version')
+    name = ChemblCharField(primary_key=True, max_length=20, help_text='Name of release version')
+    creation_date = ChemblDateField(blank=True, null=True, help_text='Date database created')
+    comments = ChemblCharField(max_length=2000, blank=True, null=True, help_text='Description of release version')
 
     class Meta(ChemblCoreAbstractModel.Meta):
         pass
@@ -37,10 +37,10 @@ class ChemblIdLookup(six.with_metaclass(ChemblModelMetaClass, ChemblCoreAbstract
         ('OBS', 'OBS'),
         )
 
-    chembl_id = ChemblCharField(primary_key=True, max_length=20, help_text=u'ChEMBL identifier')
-    entity_type = ChemblCharField(max_length=50, choices=ENTITY_TYPE_CHOICES, help_text=u'Type of entity (e.g., COMPOUND, ASSAY, TARGET)')
-    entity_id = ChemblIntegerField(length=9, help_text=u'Primary key for that entity in corresponding table (e.g., molregno for compounds, tid for targets)')
-    status = ChemblCharField(max_length=10, default=u'ACTIVE', choices=STATUS_CHOICES, help_text=u'Indicates whether the status of the entity within the database - ACTIVE, INACTIVE (downgraded), OBS (obsolete/removed).')
+    chembl_id = ChemblCharField(primary_key=True, max_length=20, help_text='ChEMBL identifier')
+    entity_type = ChemblCharField(max_length=50, choices=ENTITY_TYPE_CHOICES, help_text='Type of entity (e.g., COMPOUND, ASSAY, TARGET)')
+    entity_id = ChemblIntegerField(length=9, help_text='Primary key for that entity in corresponding table (e.g., molregno for compounds, tid for targets)')
+    status = ChemblCharField(max_length=10, default='ACTIVE', choices=STATUS_CHOICES, help_text='Indicates whether the status of the entity within the database - ACTIVE, INACTIVE (downgraded), OBS (obsolete/removed).')
 
     class Meta(ChemblCoreAbstractModel.Meta):
         unique_together = (("entity_id", "entity_type"),)
