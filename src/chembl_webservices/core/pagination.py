044dfacefab67412e8ec67450ae26ab5840cba0f
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-12-18 14:35:04 +0000

    fix byte encoding calls

--- src/chembl_webservices/core/pagination.py
+++ src/chembl_webservices/core/pagination.py
@@ -8,12 +8,11 @@ from django.db.models.query import QuerySet
 
 def encoded_dict(in_dict):
     out_dict = {}
-    for k, v in in_dict.iteritems():
-        if isinstance(v, unicode):
-            v = v.encode('utf8')
-        elif isinstance(v, str):
-            # Must be encoded in UTF-8
-            v.decode('utf8')
+    for k, v in in_dict.items():
+        if isinstance(v, (bytes, bytearray)):
+            v.decode('utf-8')
+        else:
+            v = '{0}'.format(v)
         out_dict[k] = v
     return out_dict
 
