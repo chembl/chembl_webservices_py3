2379068ffec9086e4b3a08756a6c8f466db782e0
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2019-12-19 12:51:07 +0000

    removal of unused middleware and formatting improvement for long IN()

--- src/chembl_webservices/middleware.py
+++ src/chembl_webservices/middleware.py
@@ -1,154 +1,34 @@
-# Orignal version taken from http://www.djangosnippets.org/snippets/186/
-# Original author: udfalkso
-# Modified by: Shwagroo Team and Gun.io
 
-import sys
-import os
 import re
-import hotshot, hotshot.stats
-import tempfile
-import io
 from django.db import connection
-from django.conf import settings
-import os
 
 from django.conf import settings
 
 words_re = re.compile( r'\s+' )
 
-group_prefix_re = [
-    re.compile( "^.*/django/[^/]+" ),
-    re.compile( "^(.*)/[^/]+$" ), # extract module path
-    re.compile( ".*" ),           # catch strange entries
-]
-
-
-class ProfileMiddleware(object):
-    """
-    Displays hotshot profiling for any view.
-    http://yoursite.com/yourview/?prof
-
-    Add the "prof" key to query string by appending ?prof (or &prof=)
-    and you'll see the profiling results in your browser.
-    It's set up to only be available in django's debug mode, is available for superuser otherwise,
-    but you really shouldn't add this middleware to any production configuration.
-
-    WARNING: It uses hotshot profiler which is not thread safe.
-    """
-    def process_request(self, request):
-        if (settings.DEBUG or request.user.is_superuser) and 'prof' in request.GET:
-            self.tmpfile = tempfile.mktemp()
-            self.prof = hotshot.Profile(self.tmpfile)
-
-    def process_view(self, request, callback, callback_args, callback_kwargs):
-        if (settings.DEBUG or request.user.is_superuser) and 'prof' in request.GET:
-            return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
-
-    def get_group(self, file):
-        for g in group_prefix_re:
-            name = g.findall( file )
-            if name:
-                return name[0]
-
-    def get_summary(self, results_dict, sum):
-        list = [ (item[1], item[0]) for item in list(results_dict.items()) ]
-        list.sort( reverse = True )
-        list = list[:40]
-
-        res = "      tottime\n"
-        for item in list:
-            res += "%4.1f%% %7.3f %s\n" % ( 100*item[0]/sum if sum else 0, item[0], item[1] )
-
-        return res
-
-    def summary_for_files(self, stats_str):
-        stats_str = stats_str.split("\n")[5:]
-
-        mystats = {}
-        mygroups = {}
-
-        sum = 0
-
-        for s in stats_str:
-            fields = words_re.split(s)
-            if len(fields) == 7:
-                time = float(fields[2])
-                sum += time
-                file = fields[6].split(":")[0]
-
-                if not file in mystats:
-                    mystats[file] = 0
-                mystats[file] += time
-
-                group = self.get_group(file)
-                if not group in mygroups:
-                    mygroups[ group ] = 0
-                mygroups[ group ] += time
-
-        return "<pre>" + \
-               " ---- By file ----\n\n" + self.get_summary(mystats,sum) + "\n" + \
-               " ---- By group ---\n\n" + self.get_summary(mygroups,sum) + \
-               "</pre>"
-
-    def process_response(self, request, response):
-        if (settings.DEBUG or request.user.is_superuser) and 'prof' in request.GET:
-            self.prof.close()
-
-            out = io.StringIO()
-            old_stdout = sys.stdout
-            sys.stdout = out
-
-            stats = hotshot.stats.load(self.tmpfile)
-            stats.sort_stats('time', 'calls')
-            stats.print_stats()
-
-            sys.stdout = old_stdout
-            stats_str = out.getvalue()
-
-            if response and response.content and stats_str:
-                response.content = "<pre>" + stats_str + "</pre>"
-
-            response.content = "\n".join(response.content.split("\n")[:40])
-
-            response.content += self.summary_for_files(stats_str)
-
-            os.unlink(self.tmpfile)
-
-        return response
-
-
-def terminal_width():
-    """
-    Function to compute the terminal width.
-    WARNING: This is not my code, but I've been using it forever and
-    I don't remember where it came from.
-    """
-    width = 0
-    try:
-        import struct, fcntl, termios
-        s = struct.pack('HHHH', 0, 0, 0, 0)
-        x = fcntl.ioctl(1, termios.TIOCGWINSZ, s)
-        width = struct.unpack('HHHH', x)[1]
-    except:
-        pass
-    if width <= 0:
-        try:
-            width = int(os.environ['COLUMNS'])
-        except:
-            pass
-    if width <= 0:
-        width = 80
-    return width
-
-
 class SqlPrintingMiddleware(object):
-
-    sql_format_regex = re.compile(r'\b(select|from|where|group|having|order)\b', re.IGNORECASE)
-    sql_indent_format_regex = re.compile(r'(,(?!\s:arg)|\bjoin\b|\bor\b|\band\b)', re.IGNORECASE)
     """
     Middleware which prints out a list of all SQL queries done
     for each view that is processed.  This is only useful for debugging.
     """
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        super(SqlPrintingMiddleware, self).__init__()
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
+
+    sql_format_regex = re.compile(r'\b(select|from|where|group|having|order)\b', re.IGNORECASE)
+    sql_indent_format_regex = re.compile(r'(,(?!\s:arg)|\bjoin\b|\bor\b|\band\b)', re.IGNORECASE)
+    sql_identifiers_format_regex = re.compile(r'IN\s+\(\s*(?:[^\s]+\s*,\s*)+[^\s]+\s*\)', re.IGNORECASE)
     def process_response(self, request, response):
         indentation = 2
         if len(connection.queries) > 0 and settings.DEBUG:
@@ -156,8 +36,26 @@ class SqlPrintingMiddleware(object):
             for query in connection.queries:
                 sql_parts = query['sql'].split("' - PARAMS = ")
                 sql_string = sql_parts[0].replace("QUERY = u'", '')
+                identifier_matches = SqlPrintingMiddleware.sql_identifiers_format_regex.findall(sql_string)
+
+                for id_match_i in identifier_matches:
+                    ids_i = id_match_i.split(',')
+                    num_ids = len(ids_i)
+                    hidden_ids = 10
+                    if num_ids > hidden_ids:
+                        sql_string = sql_string.replace(
+                            id_match_i,
+                            '{0} . . . <<{1} ID\'s hidden>>)'.format('<;>'.join(ids_i[:hidden_ids]), num_ids-hidden_ids)
+                        )
+                    else:
+                        sql_string = sql_string.replace(
+                            id_match_i,
+                            '<;>'.join(ids_i)
+                        )
+
                 sql_string = SqlPrintingMiddleware.sql_format_regex.sub('\n\\1', sql_string)
                 sql_string = SqlPrintingMiddleware.sql_indent_format_regex.sub('\\1\n    ', sql_string)
+                sql_string = sql_string.replace('<;>', ',')
                 print("QUERY TIME: %s secs" % query['time'])
                 print('QUERY:' + sql_string + '\n')
                 if len(sql_parts) > 1:
