7ca94fbe43ab3de97eb14c53d17c9e7e420bb442
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-01-22 16:45:31 +0000

    test case updates for chembl 26

--- src/chembl_webservices/tests/test_small_endpoints.py
+++ src/chembl_webservices/tests/test_small_endpoints.py
@@ -156,13 +156,12 @@ class OrganismTestCase(BaseWebServiceTestCase):
 
     resource = 'organism'
     id_property = 'oc_id'
-    resource_expected_count = 3847
+    resource_expected_count = 3959
     sorting_test_props = ['l1', 'l2', 'l3']
     mandatory_properties = [
       'l1',
       'l2',
       'l3',
-      'l4_synonyms',
       'oc_id',
       'tax_id',
     ]
@@ -178,97 +177,48 @@ class MoleculeFormTestCase(BaseWebServiceTestCase):
       'parent_chembl_id',
     ]
 
+class SourceTestCase(BaseWebServiceTestCase):
 
-#     def test_source_resource(self):
-#         source = new_client.source
-#         count = len(source.all())
-#         self.assertTrue(count)
-#         self.assertTrue(source.filter(src_short_name="ATLAS").exists())
-#         self.assertEqual( [src['src_id'] for src in source.all().order_by('src_id')[0:5]], [1,2,3,4,5])
-#         random_index = 5#randint(0, count - 1)
-#         random_elem = source.all()[random_index]
-#         self.assertIsNotNone(random_elem, "Can't get {0} element from the list".format(random_index))
-#         self.assertIn('src_description', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('src_id', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('src_short_name', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         source.set_format('xml')
-#         parseString(source.filter(src_short_name="DRUGS")[0])
+    resource = 'source'
+    id_property = 'src_id'
+    resource_expected_count = 47
+    mandatory_properties = [
+      'src_description',
+      'src_id',
+      'src_short_name',
+    ]
 
+class TargetComponentTestCase(BaseWebServiceTestCase):
 
+    resource = 'target_component'
+    id_property = 'component_id'
+    resource_expected_count = 9756
+    mandatory_properties = [
+      'accession',
+      'component_id',
+      'component_type',
+      'description',
+      'go_slims.go_id',
+      'organism',
+      'protein_classifications.protein_classification_id',
+      'sequence',
+      'target_component_synonyms.component_synonym',
+      'target_component_synonyms.syn_type',
+      'target_component_xrefs.xref_id',
+      'target_component_xrefs.xref_name',
+      'target_component_xrefs.xref_src_db',
+      'targets.target_chembl_id',
+      'tax_id',
+    ]
 
-#
-#     def test_target_prediction_resource(self):
-#         target_prediction = new_client.target_prediction
-#         count = len(target_prediction.all())
-#         self.assertTrue(count)
-#         self.assertTrue(set(["P15823", "P43140", "P23944", "P35368", "P18130"]).issubset(set(tar['target_accession'] for tar in target_prediction.filter(molecule_chembl_id='CHEMBL2'))))
-#         self.assertTrue(all(float(tar['probability']) >= 0.9 for tar in target_prediction.filter(molecule_chembl_id='CHEMBL3').filter(probability__gte=0.9)))
-#         self.assertEqual(len(target_prediction.filter(molecule_chembl_id='CHEMBL4').filter(probability__lte=0.5)), 95)
-#         self.assertEqual(target_prediction.filter(molecule_chembl_id='CHEMBL5').order_by('probability')[0]['target_chembl_id'], "CHEMBL5080")
-#         random_index = 7878
-#         random_elem = target_prediction.all()[random_index]
-#         self.assertIsNotNone(random_elem, "Can't get {0} element from the list".format(random_index))
-#         self.assertIn('in_training', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('molecule_chembl_id', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('pred_id', random_elem,
-#                       'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('probability', random_elem,
-#                       'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('target_accession', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('target_chembl_id', random_elem,
-#                       'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('value', random_elem,
-#                       'One of required fields not found in resource {0}'.format(random_elem))
-#         target_prediction.set_format('xml')
-#         parseString(target_prediction.filter(molecule_chembl_id='CHEMBL6').filter(value=1)[0])
+class ChemblIdLookUpTestCase(BaseWebServiceTestCase):
 
-
-#
-#
-#     def test_target_component_resource(self):
-#         target_component = new_client.target_component
-#         count = len(target_component.all())
-#         self.assertTrue(count)
-#         self.assertTrue('sequence' in target_component.get(1295))
-#         self.assertEqual([targcomp['component_id'] for targcomp in target_component.all().order_by('component_id')[0:5]],
-#             [1, 2, 3, 4, 5])
-#         has_synonyms = target_component.get(375)
-#         self.assertIn('target_component_synonyms', has_synonyms, 'One of required fields not found in resource {0}'.format(has_synonyms))
-#         synonym = has_synonyms['target_component_synonyms'][0]
-#         self.assertIn('component_synonym', synonym, 'One of required fields not found in resource {0}'.format(synonym))
-#         self.assertIn('syn_type', synonym, 'One of required fields not found in resource {0}'.format(synonym))
-#         target_component.set_format('xml')
-#         random_index = 5432  # randint(0, count - 1)
-#         random_elem = target_component.all()[random_index]
-#         self.assertIsNotNone(random_elem, "Can't get {0} element from the list".format(random_index))
-#         self.assertIn('accession', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('sequence', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('component_id', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('component_type', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('description', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('go_slims', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('organism', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('tax_id', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         parseString(target_component.all()[0])
-
-
-
-#
-#     def test_chembl_id_lookup_resource(self):
-#         chembl_id_lookup = new_client.chembl_id_lookup
-#         count = len(chembl_id_lookup.all())
-#         self.assertTrue(count)
-#         self.assertTrue(chembl_id_lookup.filter(entity_type="TARGET").exists())
-#         random_index = 5678  # randint(0, count - 1)
-#         random_elem = chembl_id_lookup.all()[random_index]
-#         self.assertNotEqual(chembl_id_lookup.all().order_by('chembl_id')[0]['chembl_id'], chembl_id_lookup.all().order_by('-chembl_id')[0]['chembl_id'])
-#         self.assertNotEqual(chembl_id_lookup.all().order_by('entity_type')[0]['chembl_id'], chembl_id_lookup.all().order_by('-entity_type')[0]['chembl_id'])
-#         self.assertNotEqual(chembl_id_lookup.all().order_by('status')[0]['chembl_id'], chembl_id_lookup.all().order_by('-status')[0]['chembl_id'])
-#         self.assertIsNotNone(random_elem, "Can't get {0} element from the list".format(random_index))
-#         self.assertIn('chembl_id', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('entity_type', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('status', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         self.assertIn('resource_url', random_elem, 'One of required fields not found in resource {0}'.format(random_elem))
-#         chembl_id_lookup.set_format('xml')
-#         parseString(chembl_id_lookup.filter(entity_type="COMPOUND").filter(status="ACTIVE")[0])
-#
+    resource = 'chembl_id_lookup'
+    id_property = 'chembl_id'
+    resource_expected_count = 3687036
+    mandatory_properties = [
+      'chembl_id',
+      'entity_type',
+      'resource_url',
+      'status',
+    ]
