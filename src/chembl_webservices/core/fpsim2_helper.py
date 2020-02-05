33a5f2b0dc4879a38acf05454b1eb200eb510488
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-02-05 17:32:48 +0000

    singleton patter for the FPSIM engine and load variables from settings

--- src/chembl_webservices/core/fpsim2_helper.py
+++ src/chembl_webservices/core/fpsim2_helper.py
@@ -1,7 +1,17 @@
 from FPSim2 import FPSim2Engine
-from django.conf import settings
+import time
 
-fpe = FPSim2Engine(settings.FPSIM2_FILE_PATH)
+# Variable loaded from the Settings to prevent circular references
+FPSIM2_FILE_PATH = None
+FPSIM_ENGINE = None
+
+def get_fpsim_engine():
+    global FPSIM_ENGINE, FPSIM2_FILE_PATH
+    if FPSIM_ENGINE is None:
+        t_ini = time.time()
+        FPSIM_ENGINE = FPSim2Engine(FPSIM2_FILE_PATH)
+        print('FPSIM2 FILE LOADED IN {0} SECS'.format(time.time()-t_ini))
+    return FPSIM_ENGINE
 
 def get_similar_molregnos(query_smiles, similarity=0.7):
     """
@@ -12,4 +22,4 @@ def get_similar_molregnos(query_smiles, similarity=0.7):
     if similarity < 0.7 or similarity > 1:
         raise ValueError('Similarity should have a value between 0.7 and 1.')
 
-    return fpe.similarity(query_smiles, similarity, n_workers=1)
+    return get_fpsim_engine().similarity(query_smiles, similarity, n_workers=1)
