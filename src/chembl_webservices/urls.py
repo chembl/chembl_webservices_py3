2c07561e936c6fcab9fba84dffed213ece3374c3
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-02-26 14:56:53 +0000

    simple target prediction proxy functionality

--- src/chembl_webservices/urls.py
+++ src/chembl_webservices/urls.py
@@ -1,5 +1,10 @@
 __author__ = 'mnowotka'
+from django.conf.urls import url
 
 from chembl_webservices.api_config import api as webservices
+from chembl_webservices.target_predictions_proxy import target_predictions_proxy
 
 urlpatterns = webservices.urls
+
+urlpatterns.append(url(r'^data/target_predictions_proxy$',
+                       target_predictions_proxy, ))
