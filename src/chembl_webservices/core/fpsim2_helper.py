c02d38299b3a9071e5323d0e01403281f963d084
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-03-04 13:07:25 +0000

    little refactor for documentation

--- src/chembl_webservices/core/fpsim2_helper.py
+++ src/chembl_webservices/core/fpsim2_helper.py
@@ -13,13 +13,13 @@ def get_fpsim_engine():
         print('FPSIM2 FILE LOADED IN {0} SECS'.format(time.time()-t_ini))
     return FPSIM_ENGINE
 
-def get_similar_molregnos(query_smiles, similarity=0.7):
+def get_similar_molregnos(query_string, similarity=0.7):
     """
-    :param query_smiles: the smiles representation of the query
+    :param query_string: the smiles, inchi or molfile representation of the query
     :param similarity: the minimum similarity threshold
     :return: a list with tuples of (molregno, similarity)
     """
     if similarity < 0.4 or similarity > 1:
         raise ValueError('Similarity should have a value between 0.4 and 1.')
 
-    return get_fpsim_engine().similarity(query_smiles, similarity, n_workers=1)
+    return get_fpsim_engine().similarity(query_string, similarity, n_workers=1)
