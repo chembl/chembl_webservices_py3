e5681461cb88e7a43bac69e0090f74c9a11a0892
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-03-06 11:47:05 +0000

    test case update

--- src/chembl_webservices/tests/test_images.py
+++ src/chembl_webservices/tests/test_images.py
@@ -10,7 +10,6 @@ class ImagesTestCase(BaseWebServiceTestCase):
                                                expected_code=400)
 
         image_binary = self.get_resource_by_id('compound_structural_alert', 30843761, custom_format='svg')
-        print(image_binary)
         self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
         image_binary = self.get_resource_by_id('compound_structural_alert', 32589079, custom_format='svg')
         self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
@@ -18,10 +17,8 @@ class ImagesTestCase(BaseWebServiceTestCase):
     def test_molecule(self):
         self.get_resource_by_id('molecule', 'CHEMBL1', custom_format='png', expected_code=400)
         self.get_resource_by_id('molecule', 'CHEMBL450200', custom_format='png', expected_code=400)
-        self.get_resource_by_id('molecule', 'CHEMBL1', custom_format='json', expected_code=400)
-        self.get_resource_by_id('molecule', 'CHEMBL450200', custom_format='json', expected_code=400)
 
         image_binary = self.get_resource_by_id('molecule', 'CHEMBL1', custom_format='svg')
-        self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg:svg version='1.1'"))
+        self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
         image_binary = self.get_resource_by_id('molecule', 'CHEMBL450200', custom_format='svg')
-        self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg:svg version='1.1'"))
+        self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
