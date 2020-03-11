31107fc4ad4812946a10dd3c09530aea6e3d5d25
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-03-11 10:06:35 +0000

    remove target prediction proxy

--- src/chembl_webservices/urls.py
+++ src/chembl_webservices/urls.py
@@ -13,7 +13,7 @@ urlpatterns = webservices.urls
 def deprecated_target_prediction_request(request):
     return HttpResponse('ERROR: The target prediction endpoint has been removed in ChEMBL 26.\n', status=410)
 
-urlpatterns.append(url(r'^data/target_predictions_proxy$',
-                       target_predictions_proxy, ))
+# urlpatterns.append(url(r'^data/target_predictions_proxy$',
+#                        target_predictions_proxy, ))
 urlpatterns.append(url(r'^data/target_prediction.*$',
                        deprecated_target_prediction_request, ))
