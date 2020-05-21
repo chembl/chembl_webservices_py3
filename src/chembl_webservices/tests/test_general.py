5a78955c575d6a5689ae31eb883844f000e58b39
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-05-21 13:29:04 +0100

    updated tests cases to match ChEMBL 27 release

--- src/chembl_webservices/tests/test_general.py
+++ src/chembl_webservices/tests/test_general.py
@@ -7,10 +7,10 @@ class GeneralTestCase(BaseWebServiceTestCase):
         status_res = self.request_url(self.WS_URL + '/status.json')
         message = 'Invalid status response!'
         self.assertEqual(status_res['activities'], 16066124, message)
-        self.assertEqual(status_res['chembl_db_version'], 'ChEMBL_26', message)
-        self.assertEqual(status_res['chembl_release_date'], '2020-01-10T00:00:00', message)
+        self.assertEqual(status_res['chembl_db_version'], 'ChEMBL_27', message)
+        self.assertEqual(status_res['chembl_release_date'], '2020-05-18T00:00:00', message)
         self.assertEqual(status_res['compound_records'], 2444828, message)
         self.assertEqual(status_res['disinct_compounds'], 1961462, message)
         self.assertEqual(status_res['publications'], 76086, message)
         self.assertEqual(status_res['status'], 'UP', message)
-        self.assertEqual(status_res['targets'], 13377, message)
+        self.assertEqual(status_res['targets'], 13382, message)
--- src/chembl_webservices/tests/test_images.py
+++ src/chembl_webservices/tests/test_images.py
@@ -4,14 +4,14 @@ from chembl_webservices.tests import BaseWebServiceTestCase
 class ImagesTestCase(BaseWebServiceTestCase):
 
     def test_compound_structural_alert(self):
-        self.get_resource_by_id('compound_structural_alert', 30843761, custom_format='png',
+        self.get_resource_by_id('compound_structural_alert', 34534481, custom_format='png',
                                                expected_code=400)
-        self.get_resource_by_id('compound_structural_alert', 32589079, custom_format='png',
+        self.get_resource_by_id('compound_structural_alert', 34534482, custom_format='png',
                                                expected_code=400)
 
-        image_binary = self.get_resource_by_id('compound_structural_alert', 30843761, custom_format='svg')
+        image_binary = self.get_resource_by_id('compound_structural_alert', 34534481, custom_format='svg')
         self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
-        image_binary = self.get_resource_by_id('compound_structural_alert', 32589079, custom_format='svg')
+        image_binary = self.get_resource_by_id('compound_structural_alert', 34534482, custom_format='svg')
         self.assertTrue(image_binary.startswith("<?xml version='1.0' encoding='iso-8859-1'?>\n<svg version='1.1'"))
 
     def test_molecule(self):
