fe2fb3b2e0e076029ed280b814d42f2eb904a104
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-05-21 12:53:33 +0100

    proxy for target predictions is no longer required

--- src/chembl_webservices/urls.py
+++ src/chembl_webservices/urls.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from chembl_webservices.api_config import api as webservices
-from chembl_webservices.target_predictions_proxy import target_predictions_proxy
 
 urlpatterns = webservices.urls
 
@@ -13,7 +12,5 @@ urlpatterns = webservices.urls
 def deprecated_target_prediction_request(request):
     return HttpResponse('ERROR: The target prediction endpoint has been removed in ChEMBL 26.\n', status=410)
 
-# urlpatterns.append(url(r'^data/target_predictions_proxy$',
-#                        target_predictions_proxy, ))
 urlpatterns.append(url(r'^data/target_prediction.*$',
                        deprecated_target_prediction_request, ))
